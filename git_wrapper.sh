2eb05c1b6126ac3072ed99042ab20d5e72a3dfba
xsj3n <xsj3n@tutanota.com>
2025-06-08 23:48:30 -0700

    settling on this

--- git_wrapper.sh
+++ git_wrapper.sh
@@ -3,49 +3,34 @@ if [[ "push" != "$1" ]] ; then
   exit "$?"
 fi
 
-# TODO: check this logic once further progressed 
-check_for_gpg() {
-  if [[ ${1: -3} == "gpg" ]]; then
-    return 0
-  else
-    return 1
-  fi
-}
-
 directory_decrypt() {
-  for file in *.gpg; do
+  mapfile -t gpg_files < <(find . -type f -name "*.gpg")
+  for file in "${gpg_files[@]}"; do
     gpg --decrypt "$file" > "${file:0:-4}"
+    touch -r "$file" "${$file:0:-4}"
     rm "$file"
   done
 }
 
 # encrypt every file matching the pattern, skipping those that end w/ .gpg
-# $1 = pattern | $2+ = staged_files
+# $1 = pattern 
 encrypt_pattern() {
-  local -n staged_files_ref="$staged_files" &>/dev/null
-  mapfile -t repo_files < <(find . -type f -name "$1" | xargs -n1 basename 2>/dev/null)
+  mapfile -t repo_files < <(find . -type f -name "$1")
   for file in "${repo_files[@]}"; do
-    [[ "${staged_files_ref[*]}" == "$file.gpg" ]] && continue 
- 
+    [[ "${$file:0:-4}" == ".gpg" ]] && continue 
     gpg --encrypt --recipient xsj3n@tutanota.com --yes --trust-model always --output "$file.gpg" "$file"
 
     echo " encrypt mode - $file"
-    git rm --cached "$file" &>/dev/null
-    
+    git rm --cached "$file" &>/dev/null 
     git add "$file.gpg"
   done
 }
 
 
 echo "secret check starting..."
-# make sure glob expands to null w/o match
 shopt -s nullglob
 repo_root="$(git rev-parse --show-toplevel)"
-
-# may need to check if files are staged already but thats an issue for later 
-mapfile -t staged_files < <(git diff --name-only --cached | xargs -n1 basename &>/dev/null)
 mapfile -t secret_file_patterns < "$repo_root/.gitsecret"
-
 
 for pattern in "${secret_file_patterns[@]}"; do
   encrypt_pattern "$pattern" staged_files
@@ -53,10 +38,4 @@ done
 
 git commit --amend --no-edit
 git push "${@:2}"
-
-# check for .gpg files, if for some reason, the files were not encrypted, 
-gpg_files=(*.gpg)
-if [[ ${#gpg_files[@]} -eq 0  ]]; then
-  exit
-fi
-rm ./*.gpg
+directory_decrypt
