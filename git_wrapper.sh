f7009bd1fc5671d76727c2b696b67cba063e0578
xsj3n <xsj3n@tutanota.com>
2025-06-08 20:03:16 -0700

    test

--- git_wrapper.sh
+++ git_wrapper.sh
@@ -30,7 +30,7 @@ directory_decrypt() {
 }
 
 # encrypt every file matching the pattern, skipping those that end w/ .gpg
-# $1 = pattern | $2+ = staged_files 
+# $1 = pattern | $2+ = staged_files
 encrypt_pattern() {
   local -n staged_files_ref="$staged_files"
   mapfile -t repo_files < <(find . -type f -name "$1" | xargs -n1 basename 2>/dev/null)
@@ -38,18 +38,18 @@ encrypt_pattern() {
     [[ "${staged_files_ref[*]}" == "$file.gpg" ]] && continue 
  
     gpg --encrypt --recipient xsj3n@tutanota.com --yes --trust-model always --output "$file.gpg" "$file"
-    rm "$file"
 
-    git reset HEAD "$file" &>/dev/null
+    git restore --staged "$file"
+    rm "$file"
     git add "$file.gpg"
   done
 }
 
 
 
-repo_root="$(git rev-parse --show-toplevel)"
 # make sure glob expands to null w/o match
 shopt -s nullglob
+repo_root="$(git rev-parse --show-toplevel)"
 
 # may need to check if files are staged already but thats an issue for later 
 mapfile -t staged_files < <(git diff --name-only --cached | xargs -n1 basename)
@@ -60,15 +60,12 @@ for pattern in "${secret_file_patterns[@]}"; do
   encrypt_pattern "$pattern" staged_files
 done
 
-
 git commit --amend --no-edit
 git push "${@:2}"
-
 
 # check for .gpg files, if for some reason, the files were not encrypted, 
 gpg_files=(*.gpg)
 if [[ ${#gpg_files[@]} -eq 0  ]]; then
   exit
 fi
-
 directory_decrypt
